df77cfda21cf3a6e68086b194af633c6bc9a43d7
tjhirani <60822139+tjhirani@users.noreply.github.com>
2020-04-08 20:26:22 +0530

    Update bomber.py

--- bomber.py
+++ bomber.py
@@ -562,7 +562,7 @@ elif type == 0:
             input("Enter Delay time (in seconds) [Recommended 10 sec ] : "))
 maxlim = 0
 if cc == "91":
-    maxlim = 5000
+    maxlim = 500
 else:
     maxlim = 100
 if nm > maxlim:
